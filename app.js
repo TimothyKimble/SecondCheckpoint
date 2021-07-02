a0c8a7e137fa8caf82795c8bece344ac8c116643
Timothy Kimble <timble417@gmail.com>
2021-07-02 11:59:29 -0600

    added header main and footer. Made function notes

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log('hello World')
+
+
+let experience = 0
+let clicks = 1
+let sword = 0
+let enchant = 0
+let blade = 0
+let lucky = 0
+
+
+
+// NOTE This function will be the clicker for the player. When they click on the picture, add experience.
+function clickButton() {
+  experience += 1
+
+  console.log(experience)
+
+}
+
+// NOTE This function should update the current experience available to the player.
+function update() {
+
+}
+
+// NOTE When Upgrade Sword button is clicked, increase the experience gained for each click by 1.5 times. Also increase the cost of the button
+function upgradesword() {
+
+}
+
+
+// NOTE When Apply Enchantment is clicked, increase the experienced gained by each click by 3 times also increase the cost of the button
+function ApplyEnchantment() {
+
+}
+
+
+
+clickbutton()
+alert(experience)
